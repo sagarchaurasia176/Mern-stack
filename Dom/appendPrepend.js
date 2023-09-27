e4f89ada35385861db4e615560e2f5be2aee1c64
Sagar Chaurasia <101509099+sagarchaurasia176@users.noreply.github.com>
2023-09-27 18:07:53 +0530

    Delete Dom directory

--- Dom/appendPrepend.js
+++
@@ -1,7 +0,0 @@
-let btn = document.querySelector('#f1');
-
-btn.onclick = () => {
-    let text = document.createElement('p');
-    text.innerText = 'sagar';
-    document.body.prepend(text);
-}
--- Dom/dom.js
+++
@@ -1,5 +0,0 @@
-const btn = document.querySelector('button');
-
-btn.addEventListener('click', () => {
-    btn.innerHTML = 'Hello<b>sagar</b';
-})
