69cb31b38918a82376966092ad532409376588a9
Sagar Chaurasia <101509099+sagarchaurasia176@users.noreply.github.com>
2023-11-20 23:36:45 +0530

    Javascript challenge 60 days

---
+++ js/obj.js
@@ -0,0 +1,62 @@
+const data = {
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+
+    "address": {
+        "street": "Kulas Light",
+        "suite": "Apt. 556",
+        "city": "Gwenborough",
+        "zipcode": "92998-3874",
+        "geo": {
+            "lat": "-37.3159",
+            "lng": "81.1496"
+        }
+
+    },
+}
+let dataCheck = (id, name, username, email) => {
+    let userId = console.log(this.id);
+    let userName = console.log(this.name);
+};
+dataCheck.userId;
+dataCheck.userName;
+
+
+
+//     data.forEach(userData => {
+//         console.log(userData.id);
+//         console.log(userData.name);
+//         console.log(userData.username);
+//         console.log(userData.email);
+//     });
+// };
+
+// console.log(dataCheck());
+//     "phone": "1-770-736-8031 x56442",
+//     "website": "hildegard.org",
+//     "company": {
+//         "name": "Romaguera-Crona",
+//         "catchPhrase": "Multi-layered client-server neural-net",
+//         "bs": "harness real-time e-markets"
+//     }
+// },
+
+// {
+//     "id": 2,
+//     "name": "Ervin Howell",
+//     "username": "Antonette",
+//     "email": "Shanna@melissa.tv",
+//     "address": {
+//         "street": "Victor Plains",
+//         "suite": "Suite 879",
+//         "city": "Wisokyburgh",
+//         "zipcode": "90566-7771",
+//         "geo": {
+//             "lat": "-43.9509",
+//             "lng": "-34.4618"
+//         }
+//     }
+// }
+console.log(dataCheck);
