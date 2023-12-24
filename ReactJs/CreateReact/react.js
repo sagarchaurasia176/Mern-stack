44951e131766632eacf4906f0b5b8eccdc4b142b
Sagar Chaurasia <101509099+sagarchaurasia176@users.noreply.github.com>
2023-12-25 01:16:35 +0530

    Complete React js Practice code

---
+++ ReactJs/CreateReact/react.js
@@ -0,0 +1,5 @@
+let data = React.createElement('h1', {}, 'hellow react ');
+ReactDOM.createRoot(document.getElementById('text')).render(data);
+
+const codingImg = ReactDOM.createRoot(document.getElementById("imge"))
+    .render(codingImg);
