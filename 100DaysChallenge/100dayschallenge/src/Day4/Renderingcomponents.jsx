77aa01157afda230ef3a8d75772a14ae1fd9db5d
sagarchaurasia176 <sagarchaurasia176@gmail.com>
2024-01-01 17:58:50 +0530

    Day 4

--- 100DaysChallenge/100dayschallenge/src/Day4/Renderingcomponents.jsx
+++ 100DaysChallenge/100dayschallenge/src/Day4/Renderingcomponents.jsx
@@ -1,33 +1,62 @@
 import React from "react";
-import CssBaseline from "@mui/material/CssBaseline";
-import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
-
+import "./main.css";
 export default function Renderingcomponents() {
   const mealFood = async (query) => {
     try {
-      const foodApi =
-        "https://www.themealdb.com/api/json/v1/1/search.php?s={query}";
-      const foodApiConvrt = await fetch(foodApi);
-      const response = foodApiConvrt.json();
-      //   console.log(response);
+      const dataApi = await fetch(
+        `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`
+      );
+      const response = await dataApi.json();
+      console.log(response);
     } catch {
       throw new Error("!oops ");
     }
     // last stopsed of the functions
   };
+  //btn response
 
-  mealFood();
+  function btnClick() {
+    let userRespon = document.querySelector(".search-input");
+    if (userRespon) {
+      let dataGet = userRespon.value.trim();
+      mealFood(dataGet);
+    }
+  }
+
   return (
     <>
-      <React.Fragment>
-        <CssBaseline />
-        <Container maxWidth="md">
-          <Box sx={{ bgcolor: "#000000", height: "50vh" }} />
-    
-        </Container>
-
-      </React.Fragment>
+      <div className="container">
+        <header>
+          <h1>Foody corner</h1>
+        </header>
+        <search className="search">
+          <input type="text" class="search-input" placeholder="Search..." />
+          <button
+            type="submit"
+            class="search-button"
+            id="searcBtn"
+            onClick={btnClick}
+          >
+            Search
+          </button>
+        </search>
+        <table className="table-auto ">
+          <thead>
+            <tr>
+              <th>Meal Id</th>
+              <th>Food Name</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>The Sliding Mr</td>
+              <td>Malcol</td>
+              <td>1961</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
