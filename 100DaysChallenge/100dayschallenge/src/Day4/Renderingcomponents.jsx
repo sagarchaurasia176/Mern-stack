1cb58e0c3c7b309fef54ae8376bc6c12b749a0f4
sagarchaurasia176 <sagarchaurasia176@gmail.com>
2024-01-06 16:25:33 +0530

    Project Done

--- 100DaysChallenge/100dayschallenge/src/Day4/Renderingcomponents.jsx
+++ 100DaysChallenge/100dayschallenge/src/Day4/Renderingcomponents.jsx
@@ -1,24 +1,41 @@
 import React from "react";
 import "./main.css";
 import { useState } from "react";
+let processing = document.createElement('p');
 async function ApiFoodCorner(query) {
+  processing.innerHTML = "your item is searching ....";
+  document.querySelector('.container').appendChild(processing);
+
   try {
     const ApiFetchUrl = await fetch(
-      `https://www.themealdb.com/api/json/v1/1/search.php?f=${query}`
+      ` https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`
     );
+    processing.innerHTML = '';
+
     const urlConvrt = await ApiFetchUrl.json();
+    return urlConvrt.meals;
   } catch {
     throw new Error("!oops error");
   }
 }
-const btnClickHandler = () => {
-    let userInput = document.getElementById('searchInput').value.trim();
-    console.log(ApiFoodCorner(userInput))
-  }
-
 export default function DisplayData() {
-  // dataStored();
-  const [meals, setMeals] = useState([]);
+  
+  const [meals, setMeal] = useState([]);
+
+  const btnClickHandler = async () => {
+    let userInput = document.getElementById("searchInput").value.trim();
+    try {
+      const data = await ApiFoodCorner(userInput);
+      setMeal(data);
+
+    } catch (error) {
+      // Handle error if needed
+      console.log("!oops");
+    }
+  };
+
+
+
   return (
     <>
       <div className="container">
@@ -41,24 +58,31 @@ export default function DisplayData() {
             Search
           </button>
         </search>
+
         <table className="table-auto ">
           <thead>
             <tr>
               <th>Meal Id</th>
               <th>Food Img</th>
-              <th>Price</th>
+              <th>strArea</th>
+              <th>Category</th>
             </tr>
           </thead>
           <tbody>
-            {meals.map((item)=>{
-               <tr id={item.idMealid}>
-               <td>{item.idMealid}</td>
-               <td>{item.strIngredient1}</td>
-               <td>{item.strMealThumb}</td>
-             </tr>
-            })}
-           
+      
+            {meals.map((item) => (
+              <tr id={item.idMeal}>
+                <td>{item.idMeal}</td>
+                <td>
+                  <img src={item.strMealThumb} />
+                </td>
+                <td>{item.strArea}</td>
+                <td>{item.strCategory}</td>
+              </tr>
+            ))}
+          
           </tbody>
+
         </table>
       </div>
     </>
--- 100DaysChallenge/100dayschallenge/src/Day8/ScoreApp.jsx
+++ 100DaysChallenge/100dayschallenge/src/Day8/ScoreApp.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-export default function Score(){
-        return(
-            <>
-                {/* start to apply the event  */}
-            
-            </>
-        )
+import React from "react";
+import { useState } from "react";
+import './index.css';
+export default function Score() {
+    const [score , scoreCount] = useState(0);
+
+    function addNum(){
+        scoreCount(score + 1);
+}
+  return (
+    <>
+      {/* start to apply the event  */}
+      <div className="container">
+        <h1>Score Kepper</h1>
+            <p className=" text-center font-medium via-black  flex">{score} / {scoreCount}</p>
+        <div className="btn">
+          <button onClick={addNum}>0</button>
+          
+          <button>wicket</button>
+
+        </div>
+      </div>
+    </>
+  );
 }
