40e60bbea745b100c9a8987f6ad91645504e8a04
sagarchaurasia176 <sagarchaurasia176@gmail.com>
2024-01-27 03:54:07 +0530

    Code updated

--- 100DaysChallenge/Day28/stateConcepts/src/TextArea.jsx
+++ 100DaysChallenge/Day28/stateConcepts/src/TextArea.jsx
@@ -1,32 +1,61 @@
-import React from 'react'
-import './App.css'
+import React, { useCallback, useRef, useState } from "react";
+import "./App.css";
 export default function TextArea(props) {
-    //it's in the form of objects
-    const { heading } = props;
+  //it's in the form of objects
+  const { heading } = props;
 
+  const [text, setText] = useState("");
+  const upperCaseHandler = () => {
+    setText(text.toUpperCase());
+  };
+  const LowerCaseHandler = () => {
+    setText(text.toLowerCase());
+  };
+  //copy text
+  const textCopyVlau = useRef(undefined);
+  const copyToClip = useCallback(() => {
+    textCopyVlau.current?.select();
+    alert(" 😁 text are copied !");
+    window.navigator.clipboard.writeText(text);
+  }, [text]);
 
-    return (
-        <>
+  return (
+    <>
+      <div className=" container w-7/12 h-screen m-auto p-1">
+        <h1 className=" text-center text-lg text-white font-mono ">
+          {heading}
+        </h1>
+        <textarea
+          className=" justify-center ml-0 h-auto outline-dashed max-[1200px] p-1"
+          rows={25}
+          cols={100}
+          value={text}
+          onChange={(userTextTarget) => {
+            setText(userTextTarget.target.value);
+          }}
+        ></textarea>
 
-            <div className=' container w-7/12 h-screen m-auto p-1'>
-                <h1 className=' text-center text-lg text-white font-mono '>
-                    {heading}
-                </h1>
-                <textarea
-                    className=' justify-center ml-0 h-auto outline-dashed max-[1200px] p-1'
-                    rows={25} cols={100}>
-                </textarea>
-
-                <div className='forBtn flex justify-between '>
-                    <button className=' bg-black text-white p-2 rounded-r-md w-23  m-auto'>Convert To UpperCase</button>
-                    <button className='  bg-red-600 text-white p-2 rounded-r-md w-23 m-auto'>Convert To LowerCase</button>
-                    <button className='text-white bg-blue-400 p-2 rounded-r-md w-23 m-auto'>Copy Text</button>
-
-                </div>
-            </div>
-
-
-        </>
-    )
-
+        <div className="forBtn flex justify-between ">
+          <button
+            onClick={upperCaseHandler}
+            className=" bg-black text-white p-2 rounded-r-md w-23  m-auto"
+          >
+            Convert To UpperCase
+          </button>
+          <button
+            onClick={LowerCaseHandler}
+            className="  bg-red-600 text-white p-2 rounded-r-md w-23 m-auto"
+          >
+            Convert To LowerCase
+          </button>
+          <button
+            onClick={copyToClip}
+            className="text-white bg-blue-400 p-2 rounded-r-md w-23 m-auto"
+          >
+            Copy Text
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
