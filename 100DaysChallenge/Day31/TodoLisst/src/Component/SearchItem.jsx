86a97c67cc8cb7f36877433e1dffe7b0c7d73303
sagarchaurasia176 <sagarchaurasia176@gmail.com>
2024-01-30 15:18:12 +0530

    code updated

--- 100DaysChallenge/Day31/TodoLisst/src/Component/SearchItem.jsx
+++ 100DaysChallenge/Day31/TodoLisst/src/Component/SearchItem.jsx
@@ -1,26 +1,65 @@
-import React from 'react'
+import React, { useCallback, useState } from "react";
 export default function SearchItem() {
-
-    const Handler = () => {
-        let text = document.querySelector('#dataText');
-        let convert = text.value;
-        
-        let valueShow = document.createElement('p');
-        valueShow.innerHTML = `<p>${convert} </p> `;
-        text.appendChild(valueShow)
+    //this state is basically handle the input text
+    const [userTextWritten, setUser] = useState('');
+    //this state is basically handle the functions while the users click
+    const [userInput, setInput] = useState([])
+    //this btn is mostly used as for the getting the arrays of the obj by using the spread operator     
+    const addBtn = () => {
+        if(!useCallback){
+            console.log("empty...")
+        }else{
+                setInput([...userInput , userTextWritten]);
+        }
     }
+    const dltItem = (currIndx) => {
+        const update = userInput.filter((ele, ind) => {
+            return ind != currIndx;
+        })
+
+        setInput(update);
+    }
+
     return (
         <>
-            <div className="seacthBox flex justify-evenly">
-                <input type="text" placeholder='write your work here'
-                    id='dataText'
-                    className='p-1 outline-dotted w-2/3 outline-none'
+            <div className="seacthBox shadow-xl ">
+                <input
+                    type="text"
+                    placeholder="write your work here"
+                    id="dataText"
+                    value={userTextWritten}
+                    //onchange is mostly used into the input and inside the input we should do by applying of the onChange !
+                    onChange={(e) => {
+                        setUser(e.target.value);
+                    }}
+
+
+                    className="p-1 outline-dotted w-10/12 text-blue-700  outline-none"
                 />
                 <button
-                    onClick={Handler}
-                    className=' bg-black text-white text-center p-2 rounded-xl w-28'>add</button>
-            </div>
+                    onClick={addBtn}
 
+                    className=" bg-black text-white text-center p-2 rounded-xl w-23"
+                >
+                    <i className="fa-solid fa-plus">✍️</i>
+                </button>
+                <br />
+
+                {userInput.map((el, currIndx) => {
+                    return (
+                        <div className="box-of-list p-2  capitalize shadow-lg flex justify-between
+                                  bg-slate-400 my-4 text-black font-bold rounded-r-md" key={currIndx}>
+                            {el}
+                            <i className="fa-solid fa-trash cursor-pointer text-red-700 bg-white rounded p-2"
+                                onClick={() => dltItem(currIndx)}></i>
+                        </div>
+                    )
+                })}
+                {/* <button
+                    // onClick={dltAllItem}
+                    className=" text-black justify-center flex text-center p-2 rounded-xl w-23"
+                >Remove All</button> */}
+            </div>
         </>
-    )
+    );
 }
--- 100DaysChallenge/Day31/TodoLisst/src/Component/TodoList.jsx
+++ 100DaysChallenge/Day31/TodoLisst/src/Component/TodoList.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import SearchItem from './SearchItem'
+import './Comp.css'
 export default function TodoList() {
   return (
     <div>
         <div className="todoBox  w-1/2 h-auto  m-auto p-2 bg-white my-2">
+            <h2 className=' text-center p-2 font-medium  text-red-800 capitalize '> Todify | Add Your Today Works | </h2>
                 <SearchItem/>
                 <div className="card-1 flex p-1 justify-evenly">
                 </div>
