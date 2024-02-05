7f70a98bc7c46f35306c24ddab2f95fe6731f0d1
sagarchaurasia176 <sagarchaurasia176@gmail.com>
2024-02-05 08:39:28 +0530

    Day 37

---
+++ 100DaysChallenge/100dayschallenge/Day37/RouterConcepts/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Navbar from './Navbar'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Navbar/>
+    </>
+  )
+}
+
+export default App
