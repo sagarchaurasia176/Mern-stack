7203794579e5017a469b86c76a629bca68afc377
sagarchaurasia176 <sagarchaurasia176@gmail.com>
2024-02-08 11:36:34 +0530

    code changed

--- 100DaysChallenge/100dayschallenge/Day40/UseStateUseEffect/src/App.jsx
+++ 100DaysChallenge/100dayschallenge/Day40/UseStateUseEffect/src/App.jsx
@@ -1,13 +1,25 @@
 
 import "./App.css";
-
+import apiUrl from "./Component/Data";
 function App() {
-  return 
-  <>
-    
 
 
-  </>
+  const apiGet = async () => {
+    const apidata = await fetch(apiUrl);
+    const respo = await apidata.json();
+    console.log(respo)
+  }
+
+   
+
+  return (
+    <>
+
+      <button onClick={apiGet}>click</button>
+    </>
+  )
+  //api fetching
+
 }
 
 export default App;
