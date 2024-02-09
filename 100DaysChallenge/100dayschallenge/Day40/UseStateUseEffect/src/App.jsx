18b13d4f51b1c28946042f29ca3a68211f478159
sagarchaurasia176 <sagarchaurasia176@gmail.com>
2024-02-09 16:49:37 +0530

    code updated

--- 100DaysChallenge/100dayschallenge/Day40/UseStateUseEffect/src/App.jsx
+++ 100DaysChallenge/100dayschallenge/Day40/UseStateUseEffect/src/App.jsx
@@ -1,25 +1,23 @@
-
+import { useEffect } from "react";
 import "./App.css";
 import apiUrl from "./Component/Data";
 function App() {
-
-
   const apiGet = async () => {
     const apidata = await fetch(apiUrl);
     const respo = await apidata.json();
-    console.log(respo)
-  }
+    console.log(respo);
+  };
 
-   
+  useEffect(() => {
+    let fetch = apiUrl;
+  });
 
   return (
     <>
-
       <button onClick={apiGet}>click</button>
     </>
-  )
+  );
   //api fetching
-
 }
 
 export default App;
---
+++ 100DaysChallenge/100dayschallenge/Day41/my-project/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import State from "./Component/State";
+
+function App() {
+return(
+    <>
+      <State/>
+    </>
+)
+
+}
+
+export default App;
