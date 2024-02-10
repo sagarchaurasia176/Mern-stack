bbf73ee0ac102883e3ed3a8282fd03702e1c39f1
sagarchaurasia176 <sagarchaurasia176@gmail.com>
2024-02-10 19:39:07 +0530

    code updated

---
+++ 100DaysChallenge/100dayschallenge/Day42/vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import RandomUser from '../../../Day41/my-project/src/Component/RandomUser'
+function App() {
+  return (
+    <>
+  <RandomUser/>
+    </>
+  )
+}
+
+export default App
