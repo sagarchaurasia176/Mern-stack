4f5e5884c5893beda377469ee05df6ec1e02109f
sagarchaurasia176 <sagarchaurasia176@gmail.com>
2024-02-15 19:03:29 +0530

    code is updaed day6

--- 100DaysChallenge/100dayschallenge/Day46/vite-project/src/Components/MainUi.jsx
+++ 100DaysChallenge/100dayschallenge/Day46/vite-project/src/Components/MainUi.jsx
@@ -2,44 +2,51 @@ import React, { useEffect, useState } from "react";
 import Buttons from "./Button";
 import btnData from "./ButtonData";
 import FinderSearch from "./FinderSearch";
+import CardsData from "./CardsData";
 // import CardMap from "./CardMap";
 
 function MainUi() {
-    const apiKey = "www.themealdb.com/api/json/v1/1/search.php?s=";
-    //data get
-    const [query, setQuery] = useState("");
-    const [apiCheck, setCheck] = useState([]);
-    const apiData = async () => {
-        const url = apiKey + query;
-        const dataLink = await fetch(url);
-        const resp = await dataLink.json();
-        console.log(resp);
-        setCheck(resp.meals);
-    };
+  //query
+  const [query, setQuery] = useState("");
+  const [receipes, setReceipes] = useState([]);
+  // const[data , setDat] = useState();
 
-    //USEeFFECT CONCEPTS
-    useEffect(() => {
-        apiData();
-    }, []);
-    return (
-        <>
-            <div className="btnBar w-full p-2  bg-slate-900 ">
-                {/* btn data values  */}
-                <h2 className=" font-semibold text-lg text-white">Foody ❤️ Hub</h2>
-                <div
-                    className="btn m-auto text-center
+  const apiKey = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+  
+  const dataFetch = async () => {
+    const resp = apiKey + query;
+    const apiUrl = await fetch(resp);
+    const conv = await apiUrl.json();
+    console.log(conv);
+    setReceipes(conv);
+  };
+  //USEeFFECT CONCEPTS
+
+  useEffect(() => {
+    dataFetch();
+  }, []);
+
+
+
+  return (
+    <>
+      <div className="btnBar w-full p-2  bg-slate-900 ">
+        {/* btn data values  */}
+        <h2 className=" font-semibold text-lg text-white">Foody ❤️ Hub</h2>
+        <div
+          className="btn m-auto text-center
                  text-black font-medium  p-1"
-                >
-                    <Buttons btnData={btnData} />
-                </div>
-            </div>
-            <br></br>
-            <FinderSearch />
-            <br></br>
-            {/* <CardMap apiCheck={apiCheck} /> */}
-            {/* <CardsData /> */}
-        </>
-    );
+        >
+          <Buttons btnData={btnData} />
+        </div>
+      </div>
+      <br></br>
+      <FinderSearch />
+      <br></br>
+      <CardsData/>
+
+    </>
+  );
 }
 
 export default MainUi;
---
+++ 100DaysChallenge/100dayschallenge/Day47/CodeUpdate.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import MainUi from '../Day46/vite-project/src/Components/MainUi'
+function CodeUpdate() {
+  return (
+    <div>
+        {/* <MainUi/> */}
+      {/* WORK ON DAY 46 PROJECTS */}
+    </div>
+  )
+}
+
+export default CodeUpdate
