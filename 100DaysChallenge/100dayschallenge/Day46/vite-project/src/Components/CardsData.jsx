c74b614f63067151216a3c08bc5ff94607d28c42
sagarchaurasia176 <sagarchaurasia176@gmail.com>
2024-02-15 19:09:33 +0530

    Code updated

--- 100DaysChallenge/100dayschallenge/Day46/vite-project/src/Components/CardsData.jsx
+++ 100DaysChallenge/100dayschallenge/Day46/vite-project/src/Components/CardsData.jsx
@@ -8,7 +8,18 @@ function CardsData() {
         <div className="twice-box h-auto grid grid-cols-3 bg-slate-50">
 
           <div className="box-1 bg-black">
-              
+          <div className="box-1 bg-white ">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
           </div>
 
 
