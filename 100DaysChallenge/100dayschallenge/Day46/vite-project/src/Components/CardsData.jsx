7641dfd1eddd7f6f3818e59e7253297cbeae350e
sagarchaurasia176 <sagarchaurasia176@gmail.com>
2024-02-16 18:23:49 +0530

    code updated

--- 100DaysChallenge/100dayschallenge/Day46/vite-project/src/Components/CardsData.jsx
+++ 100DaysChallenge/100dayschallenge/Day46/vite-project/src/Components/CardsData.jsx
@@ -1,102 +1,4 @@
-// import React, { useEffect, useState } from "react";
-
-// function CardsData() {
-//   const alpha = [
-//     "A",
-//     "B",
-//     "C",
-//     "D",
-//     "E",
-//     "F",
-//     "G",
-//     "H",
-//     "I",
-//     "J",
-//     "K",
-//     "L",
-//     "M",
-//     "N",
-//     "P",
-//     "Q",
-//     "R",
-//     "S",
-//     "T",
-//     "U",
-//     "V",
-//     "W",
-//     "X",
-//     "Y",
-//     "Z",
-//   ];
-
-//   const [url, setUrl] = useState(
-//     `https://www.themealdb.com/api/json/v1/1/search.php?f=a`
-//   );
-//   const [item, setItem] = useState(null);
-//   //this is for fetching purpose.....
-//   useEffect(() => {
-//     fetch(url)
-//       .then((res) => {
-//         if (!res.ok) {
-//           throw new Error("Network response was not ok");
-//         }
-//         return res.json();
-//       })
-//       .then((data) => {
-//         setItem(data.meals);
-//       })
-//       .catch((error) => {
-//         console.error("Error fetching data:", error);
-//       });
-
-//   }, [url]);
-
-//   // this is the concepts of values
-//   const valueComp = () => {
-//     setUrl(`https://www.themealdb.com/api/json/v1/1/search.php?f=${alpha}`);
-//   };
-
-//   return (
-//     <>
-//       <div className=" container  m-auto w-5/6  rounded-lg">
-//         <div className="twice-box h-auto grid grid-cols-2 bg-slate-50">
-//           <div
-//             className="box-1 text-center
-//              font-semibold "
-//           >
-//             Click any word and see you'r food
-
-//             {alpha.map((value, index) => (
-//               <div
-//                 className="numsBox"
-//                 onClick={() => valueComp(value)}
-//                 key={index}
-//               >
-//                 <p className=" bg-yellow-100 cursor-pointer size-12">{value}</p>
-//               </div>
-//             ))}
-
-//           </div>
-
-//           {/* item is fetch */}
-
-//           <div className="box-2 grid grid-cols-3 gap-5">
-//             {item.map((val) => (
-//               <div className="card" key={val.idMeal}>
-//                 <img src={val.strMealThumb} alt={val.strMeal} />
-//               </div>
-//             ))}
-
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-// export default CardsData;
-
 import React, { useEffect, useState } from "react";
-
 function CardsData() {
   const alpha = [
     "A",
@@ -115,7 +17,6 @@ function CardsData() {
     "N",
     "P",
     "Q",
-
   ];
 
   const [url, setUrl] = useState(
@@ -141,10 +42,12 @@ function CardsData() {
   };
 
   return (
+    
+    
+    
     <div className="container m-autorounded-lg">
-      <div className="twice-box h-auto grid grid-cols-2 bg-slate-50">
+      <div className="twice-box h-auto flex  bg-slate-50">
         <div className="box-1 text-center font-semibold">
-          Click any word and see your food
           {alpha.map((value, index) => (
             <div
               className="numsBox"
@@ -155,14 +58,27 @@ function CardsData() {
             </div>
           ))}
         </div>
-        <div className="box-2 grid grid-cols-3 bg-yellow-200 gap-5">
+        
+        
+        
+        
+        
+        <div className="box-2 w-full grid grid-cols-3  gap-5">
           {item &&
             item.map((val) => (
-              <div className="card bg-slate-900 text-white text-center p-2" key={val.idMeal}>
-                <img src={val.strMealThumb} alt={val.strMeal} />
+              <div
+                className="card bg-slate-900  m-auto text-white text-center p-4"
+                key={val.idMeal}
+              >
+                <img
+                  className="  size-72 rounded-md"
+                src={val.strMealThumb} alt={val.strMeal} />
                 <span>{val.strMeal}</span>
               </div>
             ))}
+
+
+
         </div>
       </div>
     </div>
--- 100DaysChallenge/100dayschallenge/Day46/vite-project/src/Components/FinderSearch.jsx
+++ 100DaysChallenge/100dayschallenge/Day46/vite-project/src/Components/FinderSearch.jsx
@@ -3,10 +3,10 @@ import { CiSearch } from "react-icons/ci";
 
 function FinderSearch() {
   return (
-    <div className=" container flex m-auto p-2 w-2/3 ">
+    <div className=" container flex m-auto p-2">
       <input
         type="text"
-        className=" w-2/3 p-2 outline-none ml-28 "
+        className=" w-2/3 p-1 outline-none ml-28 "
         placeholder="type your  food here"
       />
       <CiSearch className="bg-white text-black size-10 cursor-pointer" />
