3e07432605e3223d9b4fd0b18e61267e88030be6
sagarchaurasia176 <sagarchaurasia176@gmail.com>
2024-02-17 20:09:27 +0530

    code updated

---
+++ 100DaysChallenge/100dayschallenge/Day49/Filter.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainUi from '../Day46/vite-project/src/Components/MainUi'
+
+function Filter() {
+  return (
+    <div>
+        <MainUi/>
+    </div>
+  )
+}
+
+export default Filter
