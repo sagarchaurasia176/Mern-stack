7636b7e57bc782c2a95e08fb9b7d271693631137
sagarchaurasia176 <sagarchaurasia176@gmail.com>
2024-02-26 17:58:53 +0530

    code

--- 100DaysChallenge/100dayschallenge/Day58/formUser/src/Form/Student.jsx
+++ 100DaysChallenge/100dayschallenge/Day58/formUser/src/Form/Student.jsx
@@ -1,17 +1,45 @@
 import React, { useState } from "react";
-import {toast} from 'react-toastify';
-
+import { toast } from "react-toastify";
 
 function Student() {
+  //useState data
+  const [userData, setData] = useState({
+    firstName: "",
+    lastName: "",
+    username: "",
+    country: "",
+    comments: "",
+    checkbox: "",
+  });
 
-    //useState data 
-    const [userData , setData] = useState({firstName:'' , lastName:'' , username:'' , password:'' , country:'' , comments:'' });
+
+  //data retrived here
+      const dataRetrived = (e) => {
+        e.preventDefault();
+
+        const{name , value , checked , type} = e.target;
+        //this () is used to get the setData here so what we gonna do here
+        setData((data)=>( {...data , [name] : type === 'checkbox' ? checked : value}    ))
+
+};
+
+  //data stor vlaues here when I caling this functions then something happend
+  const dataStor = () => {
+    console.log(userData);
+    if (!userData) {
+      toast.warning("pls fill the form");
+    } else {
+      toast.success("!you're data stored");
+    }
+  };
 
   return (
     <div>
       <div class=" bg-slate-400 p-8 m-auto  my-9 rounded shadow-md w-96">
         <h2 class="text-2xl font-semibold mb-6">Register</h2>
-        <form>
+
+        {/* registeration form  */}
+        <form onSubmit={dataStor}>
           <div class="grid grid-cols-2 gap-4 mb-4">
             <div>
               <label
@@ -23,21 +51,25 @@ function Student() {
               <input
                 type="text"
                 id="first-name"
-                name="first-name"
+                name="firstName"
+                onChange={dataRetrived}
+                value={userData.firstName}
                 class="w-full px-3 py-2 border rounded-md"
               />
             </div>
             <div>
               <label
-                for="last-name"
+                for="lastName"
                 class="block text-gray-700 font-semibold mb-2"
               >
                 Last Name
               </label>
               <input
                 type="text"
+                onChange={dataRetrived}
                 id="last-name"
-                name="last-name"
+                name="lastName"
+                value={userData.lastName}
                 class="w-full px-3 py-2 border rounded-md"
               />
             </div>
@@ -51,31 +83,24 @@ function Student() {
             </label>
             <input
               type="text"
+              onChange={dataRetrived}
               id="username"
+              value={userData.username}
               name="username"
               class="w-full px-3 py-2 border rounded-md"
             />
           </div>
+
           <div class="mb-4">
-            <label for="email" class="block text-gray-700 font-semibold mb-2">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              class="w-full px-3 py-2 border rounded-md"
-            />
-          </div>
-          <div class="mb-4">
-            <label
-              for="password"
-              class="block text-gray-700 font-semibold mb-2"
-            >
+            <label for="country" class="block text-gray-700 font-semibold mb-2">
               Country
             </label>
 
-            <select className=" w-full p-2">
+            <select
+              value={userData.country}
+              onChange={dataRetrived}
+              className=" w-full p-2"
+            >
               <option>India</option>
               <option>pakistan</option>
               <option>Japan</option>
@@ -90,32 +115,49 @@ function Student() {
               Comments
             </label>
 
-            <input type="checkbox" />
+            <input
+              type="checkbox"
+              checked={userData.checkbox}
+              onChange={dataRetrived}
+            />
             <span>
               get notified whene someine posts a comment on a positingx
             </span>
             <br></br>
-            <input type="checkbox" />
+            <input
+              type="checkbox"
+              onChange={dataRetrived}
+              checked={userData.checkbox}
+            />
             <span>get notified whene candidates apply for the job</span>
             <br></br>
 
-            <input type="checkbox" />
+            <input
+              type="checkbox"
+              onChange={dataRetrived}
+              checked={userData.checkbox}
+            />
             <span>
               get notified whene candidates accepts or reject an offer
             </span>
           </div>
 
           <div class="flex items-center mb-4">
-            <input type="checkbox" id="agree" name="agree" class="mr-2" />
+            <input
+              type="checkbox"
+              id="agree"
+              onChange={dataRetrived}
+              name="agree"
+              class="mr-2"
+              checked={userData.checkbox}
+            />
             <label for="agree" class="text-gray-700">
               I agree to the terms and conditions
             </label>
           </div>
+
           <div class="mt-6">
-            <button
-              type="submit"
-              class="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
-            >
+            <button class="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600">
               Register
             </button>
           </div>
