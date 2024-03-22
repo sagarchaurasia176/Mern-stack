a636a1ed206d19c3f53d5d0947e2971ae851f147
sagarchaurasia176 <sagarchaurasia176@gmail.com>
2024-03-22 18:05:39 +0530

    code updated

--- 100DaysChallenge/ExpressJS/ExpreessMongoDay84/Server.js
+++ 100DaysChallenge/ExpressJS/ExpreessMongoDay84/Server.js
@@ -1,8 +1,31 @@
 const express = require("express");
-const app = express()
+const app = express();
 const port = 3000;
 
+app.listen(port, () => {
+  console.log("serve start");
+});
 
-app.listen(port ,()=>{
-        console.log("serve start")
-})
+const body = require("body-parser");
+app.use(body.json());
+
+app.post("/api/data", (req, res) => {
+  const { name, email } = req.body;
+  console.log(name);
+  console.log(email);
+  res.send("data passed");
+});
+
+// connect mongose here
+const mongoose = require("mongoose");
+// mongoose connections
+mongoose.connect("mongodb://localhost:27017/JainServer", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("connection succesfull");
+  })
+  .catch((e) => {
+    console.log("error");
+  });
